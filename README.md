c0d03161bc781a58b6a6f5cf2b603a6b651d9ae2
Sonny Ding <sonnyding20@gmail.com>
2025-03-13 20:23:44 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# TCAD PLT to CSV
+
+This project is a Streamlit web application that allows users to upload `.plt` files from Sentaurus TCAD, and receive a converted CSV file.
+
+## Installation
+
+To set up the project, clone the repository and install the required dependencies:
+
+```bash
+git clone <repository-url>
+cd tcad-plt2csv
+python -m venv venv
+source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+pip install -r requirements.txt
+```
+
+## Usage
+
+To run the Streamlit web application, execute the following command:
+
+```bash
+streamlit run src/app.py
+```
+
+Once the application is running, you can upload `.plt` files. The application will process the files and provide you with the corresponding CSV outputs.
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit a pull request or open an issue for any suggestions or improvements.
+
+## Acknowledgements
+
+- [Pytaurus](https://github.com/thomashirtz/pytaurus/) for the `.plt` file parsing logic.
+- [Streamlit](https://streamlit.io/) for the web application framework.
---
+++ src/processing.py
@@ -0,0 +1,61 @@
+# import numpy as np
+# import pandas as pd
+# import os
+# import re
+
+# def to_snake_case(s):
+#     s = re.sub(r'([A-Z])', r'_\1', s)
+#     s = s.replace(' ', '_')
+#     s = s.replace('__', '_')
+#     return s.lower()
+
+# def process_file(input_file):
+#     with open(input_file) as f:
+#         data = f.read()
+#         headers = re.findall(r'"([a-zA-Z0-9 .]+)"', data)
+#         headers = [to_snake_case(header) for header in headers]
+#         data = data.split('Data {')[1].split('}')[0].split()
+#         data = np.array(data, dtype=float)
+
+#         data_dict = {header: [] for header in headers}
+#         for i, value in enumerate(data):
+#             header = headers[i % len(headers)]
+#             data_dict[header].append(value)
+
+#         dataframe = pd.DataFrame(data_dict)
+#         output_file = os.path.splitext(input_file)[0] + '.csv'
+#         print(output_file)
+#         return dataframe.to_csv(output_file, index=False)
+        
+
+# # process_file('/Users/sonnyding/Documents/code/tcad-plt2csv/nmos_idvg_01.plt')
+
+import numpy as np
+import pandas as pd
+import re
+import os
+
+def to_snake_case(s):
+    s = re.sub(r'([A-Z])', r'_\1', s)
+    s = s.replace(' ', '_')
+    s = s.replace('__', '_')
+    return s.lower()
+
+def process_file(file_obj, output_dir):
+    data = file_obj.read().decode('utf-8')
+    headers = re.findall(r'"([a-zA-Z0-9 .]+)"', data)
+    headers = [to_snake_case(header) for header in headers]
+    data = data.split('Data {')[1].split('}')[0].split()
+    data = np.array(data, dtype=float)
+
+    data_dict = {header: [] for header in headers}
+    for i, value in enumerate(data):
+        header = headers[i % len(headers)]
+        data_dict[header].append(value)
+
+    dataframe = pd.DataFrame(data_dict)
+    
+    output_file = os.path.join(output_dir, os.path.splitext(file_obj.name)[0] + '.csv')
+    
+    dataframe.to_csv(output_file, index=False)
+    return output_file
